3c2b131fdfefa0645be9fb87d8d9c3644c3baf36
Gabriela <gmmonsalve@uninorte.edu.co>
2019-06-27 00:56:48 -0500

    initial project version

---
+++ Desktop/Proyecto/init.js
@@ -0,0 +1,27 @@
+
+
+
+const config = {
+width: 320,
+height: 180,
+parent: "container",
+type: Phaser.AUTO,
+scene: {
+
+    preload: preload,
+    create: create,
+    update: update
+}
+
+}
+var game = new Phaser.Game(config);
+
+function preload(){
+    
+}
+function create(){
+    
+}
+function update(){
+    
+}
