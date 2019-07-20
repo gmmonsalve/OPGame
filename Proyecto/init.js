66f46bd5963a7204060a4410e9a13d0d7bac17bb
gmmonsalve <gmmonsalve@uninorte.edu.co>
2019-07-19 23:05:50 -0500

    animación de viñeta agregada, eventos de posición

--- Proyecto/init.js
+++ Proyecto/init.js
@@ -16,28 +16,60 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+
 function preload ()
 {
-    this.load.image('fondo','ranch.jpg');
-    this.load.image('vaca','vaquita.png');
-    this.load.image('mesa','mesita.png');
+    this.load.image('fondo','./imagenes/ranch.jpg');
+    this.load.image('vaca','./imagenes/vaquita.png');
+    this.load.image('mesa','./imagenes/mesita.png');
+    this.load.image('dialog','./imagenes/dialogo.png')
+    this.load.image('apple','./imagenes/manzana.png');
 }
 
 function create ()
 {
     this.fondo = this.add.image(330,175,"fondo");
     this.fondo.setScale(1.5);   
-    vak = this.add.sprite(400, 250, 'vaca');
+    animal = this.add.sprite(0, 250, 'vaca');
     mesita = this.add.image(400,450,'mesa');
+    apple = this.add.image(50,400,'apple');
+    apple.setScale(0.2);
     
+    
+}
+
+
+function moveAnimal(animal, speed)
+{
+    animal.x+=speed;
+}
+
+function resetAnimal(animal)
+{
+    animal.x = 0;
 }
 
 function update ()
 {
-   
-    vak.x += 2;
+   if(animal.x<400)
+   {
+    moveAnimal(animal,2);
     
-    if (vak.x > 1000){
-        vak.x = -100;
-    }
+   }else{
+    nube = this.add.image(630,100,'dialog').setAlpha(0);
+     this.add.image(nube.x,nube.y-8,'apple').setScale(0.2);
+    nube.setScale(0.5);
+    this.tweens.add({
+        targets: nube,
+        alphaTopLeft: { value: 1, duration: 5000, ease: 'Power1' },
+        alphaBottomRight: { value: 1, duration: 10000, ease: 'Power1' },
+        alphaBottomLeft: { value: 1, duration: 5000, ease: 'Power1', delay: 5000 },
+        yoyo: true,
+        loop: -1
+
+    });
+    
+    
+   }
+
 }
